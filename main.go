8d782f1309c624b68c1e5fe137405d2b7f2ef771
kilimnik <mail@kilimnik.de>
2023-12-28 18:24:02 +0100

    Done message better info

--- main.go
+++ main.go
@@ -153,6 +153,7 @@ func main() {
 
 	srv.OnBye(func(req *sip.Request, tx sip.ServerTransaction) {
 		callID, _ := req.CallID()
+		from, _ := req.From()
 
 		stateMapMutex.RLock()
 		state := stateMap[*callID]
@@ -167,7 +168,7 @@ func main() {
 		g := ((state.color / 1000) % 1000) % 256
 		b := ((state.color) % 1000) % 256
 
-		fmt.Printf("DONE %v, %d %d => %d %d %d\n", callID, state.x, state.y, r, g, b)
+		fmt.Printf("DONE %v (%v), %d %d => %d %d %d\n", callID, from.Name(), state.x, state.y, r, g, b)
 
 		con.Write([]byte(fmt.Sprintf("PX %d %d %02x%02x%02x\n", state.x, state.y, r, g, b)))
 		con.Close()
